7b4c58b1b56082cd55ab498b9085c2a71026edc5
RayKay91 <61206305+RayKay91@users.noreply.github.com>
2022-12-26 18:19:53 +0000

    chore: initial commit

--- test-app/ViewController.swift
+++ test-app/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let v = UIView()
+        v.backgroundColor = UIColor(red: 254/255, green: 0.0, blue: 0.0, alpha: 1.0)
+        v.frame.size.height = 100.0
+        v.frame.size.width = 200.0
+        v.layoutMargins = UIEdgeInsets(top: 50.0, left: 50.0, bottom: 0, right: 0)
+        v.layer.borderColor = CGColor(red: 0.0, green: 0.0, blue: 200.0, alpha: 1.0)
+        v.layer.borderWidth = 3.0
+        v.layer.cornerRadius = 15.0
+        
+        let t = UILabel(frame: v.frame)
+        t.text = "Hello world!"
+        t.textAlignment = .center
+        v.addSubview(t)
+        view.addSubview(v)
     }
 
 
